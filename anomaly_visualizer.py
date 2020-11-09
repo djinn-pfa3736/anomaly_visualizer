78bfff97d440d293e864283dec6c5d77ce612c4c
Hitoshi AFUSO <hitoshiafuso@gmail.com>
2020-11-09 13:13:23 +0900

    Implementing GUI

--- anomaly_visualizer.py
+++ anomaly_visualizer.py
@@ -2,10 +2,11 @@
 #If you need any help then contact to codersarts offcial website
 import sys
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QTableView, QTableWidget, QTableWidgetItem, QPushButton, QLineEdit, QAction, QFileDialog, QDialog, QGridLayout, QHBoxLayout
-from PyQt5.QtGui import QIcon
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+import pyper
 
 import pandas as pd
 import numpy as np
@@ -34,7 +35,7 @@ class MainWindow(QWidget):
         grid.addWidget(self.choose_button, 0, 3)
         grid.addWidget(self.load_button, 0, 4)
 
-        self.setWindowTitle('Anomary Visualizer')
+        self.setWindowTitle('Anomaly Visualizer')
         self.show()
 
     def choose_file(self):
@@ -59,20 +60,48 @@ class TableWindow(QWidget):
 
         self.table_view = QTableView(parent)
 
-        self.plot_label = QLabel("Input Column Number to Plot")
+        self.plot_label = QLabel("Input Column Number to Plot: ")
+        self.plot_label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.plot_label.setAlignment(Qt.AlignCenter)
+
         self.column_number = QLineEdit()
         self.plot_button = QPushButton("Plot", self)
 
-        grid.addWidget(self.table_view, 0, 0, 5, 3)
-        grid.addWidget(self.plot_label, 6, 0)
-        grid.addWidget(self.column_number, 6, 1)
-        grid.addWidget(self.plot_button, 6, 2)
+        grid.addWidget(self.table_view, 0, 0, 5, 5)
+        grid.addWidget(self.plot_label, 6, 0, 1, 3)
+        grid.addWidget(self.column_number, 6, 3)
+        grid.addWidget(self.plot_button, 6, 4)
+
+        self.region_label = QLabel("Input Plot Region")
+        self.region_min_label = QLabel("Min: ")
+        self.region_min_label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.region_min_label.setAlignment(Qt.AlignRight)
+
+        self.region_min = QLineEdit()
+        self.region_max_label = QLabel("Max: ")
+        self.region_max_label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.region_max_label.setAlignment(Qt.AlignRight)
+        self.region_max = QLineEdit()
+
+        grid.addWidget(self.region_label, 7, 0)
+        grid.addWidget(self.region_min_label, 7, 1)
+        grid.addWidget(self.region_min, 7, 2)
+        grid.addWidget(self.region_max_label, 7, 3)
+        grid.addWidget(self.region_max, 7, 4)
 
     def create_table(self, df):
         self.model = TableModel(df)
         self.table_view.setModel(self.model)
+        self.df = df
 
-class TableModel(QtCore.QAbstractTableModel):
+    def plot_column(self):
+        column_number = int(self.column_number.displayText)
+        # Not implemented...
+        self.column_plot_window = PlotWindow()
+        self.column_plot_window.plot_image('./selected_column.png')
+        self.column_plot_window.show()
+
+class TableModel(QAbstractTableModel):
 
     def __init__(self, data):
         super(TableModel, self).__init__()
@@ -95,6 +124,20 @@ class TableModel(QtCore.QAbstractTableModel):
                 return str(self._data.columns[section])
             elif orientation == Qt.Vertical:
                 return str(self._data.index[section])
+
+class PlotWindow(QWidget):
+    def __init__(self, parent = None):
+        super(PlotWindow, self).__init__(parent)
+
+    def plot_image(self, file_path):
+        grid = QGridLayout()
+        self.setLayout(grid)
+
+        self.image = QImage(file_path)
+        self.image_label = QLabel()
+
+        self.image_label.setPixmap(QPixmap.fromImage(image))
+
 
 app = QApplication(sys.argv)
 mainWin =MainWindow()
